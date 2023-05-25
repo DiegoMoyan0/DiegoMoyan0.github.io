c865fa1fddc05ea1b2088f1ba069c98b22cc0327
DiegoMoyan0 <diegomoyano02@gmail.com>
2023-05-25 05:45:27 -0300

    update

--- funciones.js
+++
@@ -1,8 +0,0 @@
-// Agrega un efecto de desplazamiento suave al hacer clic en los enlaces del menú
-document.querySelectorAll('nav ul li a').forEach(enlace => {
-    enlace.addEventListener('click', e => {
-        e.preventDefault();
-        const seccion = document.querySelector(e.target.getAttribute('href'));
-        seccion.scrollIntoView({ behavior: 'smooth' });
-    });
-});
