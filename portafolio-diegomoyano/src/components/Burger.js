9a0163aedeb8e85c6c6c9e46f64531d1e3f93b19
DiegoMoyan0 <diegomoyano02@gmail.com>
2023-10-28 15:16:40 -0300

    change burger

--- portafolio-diegomoyano/src/components/Burger.js
+++ portafolio-diegomoyano/src/components/Burger.js
@@ -4,51 +4,51 @@ import { FaHome, FaUser, FaProjectDiagram, FaEnvelope } from 'react-icons/fa';
 import '../static/css/burger.css'
 
 const BurgerMenu = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const node = useRef();
-  
-    const handleToggle = () => {
-      setIsOpen(!isOpen);
-    };
-  
-    const handleClick = e => {
-      if (node.current.contains(e.target)) {
-        return;
-      }
-      setIsOpen(false);
-    };
-  
-    useEffect(() => {
-      document.addEventListener('mousedown', handleClick);
-      return () => {
-        document.removeEventListener('mousedown', handleClick);
-      };
-    }, []);
-  
-    return (
-      <div ref={node} className="burger-menu-container">
-        <div className="burger-icon" onClick={handleToggle}>
-          {isOpen ? (
-            <>
-              <a href="#home">
-                <FaHome size={24} />
-              </a>
-              <a href="#about">
-                <FaUser size={24} />
-              </a>
-              <a href="#project">
-                <FaProjectDiagram size={24} />
-              </a>
-              <a href="#contact">
-                <FaEnvelope size={24} />
-              </a>
-            </>
-          ) : (
-            <GiHamburgerMenu size={24} />
-          )}
-        </div>
-      </div>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+  const node = useRef();
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
   };
-  
-  export default BurgerMenu;
+
+  const handleClick = e => {
+    if (node.current.contains(e.target)) {
+      return;
+    }
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClick);
+    return () => {
+      document.removeEventListener('mousedown', handleClick);
+    };
+  }, []);
+
+  return (
+    <div ref={node} className="burger-menu-container">
+      <div className="burger-icon" onClick={handleToggle}>
+        {isOpen ? (
+          <div className="icon-container">
+            <a href="#home">
+              <FaHome size={24} />
+            </a>
+            <a href="#about">
+              <FaUser size={24} />
+            </a>
+            <a href="#project">
+              <FaProjectDiagram size={24} />
+            </a>
+            <a href="#contact">
+              <FaEnvelope size={24} />
+            </a>
+          </div>
+        ) : (
+          <GiHamburgerMenu size={24} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default BurgerMenu;
